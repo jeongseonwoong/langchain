87adb7cd389a117c20c984f1668c02e212f4771e
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-15 20:15:26 +0900

    feat: few-shot 추가

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -4,13 +4,14 @@ from langchain_anthropic import ChatAnthropic
 from langchain_pinecone import PineconeVectorStore
 from langchain_upstage import UpstageEmbeddings
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMEssagePromptTemplate
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
+from config import answer_examples
 
 import os
 import streamlit as st
@@ -54,7 +55,8 @@ def get_dictionary_chain():
     dictionary_chain = dictionary_prompt | llm | StrOutputParser()
     return dictionary_chain
 
-def get_rag_chain():
+def get_history_retriever():
+
     llm = get_llm()
     retriever = get_retriever()
 
@@ -77,13 +79,29 @@ def get_rag_chain():
     history_aware_retriever = create_history_aware_retriever(
         llm, retriever, contextualize_q_prompt
     )
+    return history_aware_retriever
 
+def get_rag_chain():
+    llm = get_llm()
+
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{answer}"),
+        ]
+    )
+
+    few_shot_prompt = FewShotChatMEssagePromptTemplate(
+        example_prompt = example_prompt,
+        examples = answer_examples
+    )
+    
     system_prompt = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
+        "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
+        "아래에 제공된 문서를 활용해서 답변해주시고고"
+        "답변을 알 수 없다면 모른다고 답변해주세요요"
+        "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고고"
+        "2~3 문장정도의 짧은 내용의 답변을 원합니다다."
         "\n\n"
         "{context}"
     )
@@ -91,11 +109,13 @@ def get_rag_chain():
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
+            few_shot_prompt # 그동안 어떤식으로 채팅을 했다고 알려준다.
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     )
 
+    history_aware_retriever = get_history_retriever()
     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
