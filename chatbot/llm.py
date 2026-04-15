c57e0259680c1ab8b28a7e453da625799a3ab4ec
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-15 16:50:47 +0900

    refactor: 로직 메서드로 분리

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -25,9 +25,19 @@ def get_database():
 
 database = get_database()
 
-def  get_ai_message(user_message):
-    llm = ChatAnthropic(model="claude-haiku-4-5")
+def get_llm(model="claude-haiku-4-5"):
+    llm = ChatAnthropic(model = model)
+    return llm
+
+
+def get_retrieved_docs(user_message):
+    retrieved_docs = database.similarity_search(user_message, k=3) # query에 적합하게 임베딩된 결과
+    return retrieved_docs
+
+def get_new_question(user_message):
     
+    llm = get_llm()
+
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
     dictionary_prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
@@ -38,11 +48,11 @@ def  get_ai_message(user_message):
         """
     )
 
-
     newQuery_chain = dictionary_prompt | llm | StrOutputParser()
     new_question = newQuery_chain.invoke({"question": user_message})
-    retrieved_docs = database.similarity_search(user_message, k=3) # query에 적합하게 임베딩된 결과
+    return new_question
 
+def get_qa_prompt(retrieved_docs, new_question):
     prompt = f"""[Identity]
     - 당신은 최고의 한국 소득세 전문가 입니다.
     - [Context]를 참고해서 사용자의 질문에 답변해주세요
@@ -54,6 +64,9 @@ def  get_ai_message(user_message):
     Question: {new_question}
     """
 
+def  get_ai_message(user_message):
+    retrieved_docs = get_retrieved_docs(user_message) # query에 적합하게 임베딩된 결과
+    new_question = get_new_question(user_message)
+    qa_prompt = get_qa_prompt(retrieved_docs, new_question)
     ai_message = llm.invoke(prompt)
-
     return ai_message.content
