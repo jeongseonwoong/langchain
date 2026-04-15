acb85ef4baedfe3494f279bc6996d986e1a88dae
jeongseonwoong <tjsdnd1219@naver.com>
2026-04-15 19:50:47 +0900

    feat: 챗봇이 이전 대화 내용 기억할 수 있도록 변경

--- chatbot/llm.py
+++ chatbot/llm.py
@@ -2,42 +2,45 @@ from langchain_community.document_loaders import Docx2txtLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_anthropic import ChatAnthropic
 from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
-from dotenv import load_dotenv
 from langchain_upstage import UpstageEmbeddings
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from dotenv import load_dotenv
+
 import os
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st
 
 load_dotenv()
 
-@st.cache_resource
-def get_database():
-    upstage_embedding = UpstageEmbeddings(model="solar-embedding-1-large")
-    database = PineconeVectorStore(
-        embedding=upstage_embedding,
-        index_name="tax-index"
-    )
-    return database
+store = {}
 
-database = get_database()
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+def get_retriever():
+    embedding = UpstageEmbeddings(model="solar-embedding-1-large")
+    index_name = "tax-index"
+    database = PineconeVectorStore(
+        embedding=embedding,
+        index_name=index_name
+    )
+    retriever = database.as_retriever(search_kwargs={'k':4})
+    return retriever
 
 def get_llm(model="claude-haiku-4-5"):
-    llm = ChatAnthropic(model = model)
+    llm = ChatAnthropic(model=model)
     return llm
 
 
-def get_retrieved_docs(user_message):
-    retrieved_docs = database.similarity_search(user_message, k=3) # query에 적합하게 임베딩된 결과
-    return retrieved_docs
-
-def get_new_question(user_message):
-    
+def get_dictionary_chain():
     llm = get_llm()
-
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
     dictionary_prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
@@ -48,25 +51,74 @@ def get_new_question(user_message):
         """
     )
 
-    newQuery_chain = dictionary_prompt | llm | StrOutputParser()
-    new_question = newQuery_chain.invoke({"question": user_message})
-    return new_question
+    dictionary_chain = dictionary_prompt | llm | StrOutputParser()
+    return dictionary_chain
 
-def get_qa_prompt(retrieved_docs, new_question):
-    prompt = f"""[Identity]
-    - 당신은 최고의 한국 소득세 전문가 입니다.
-    - [Context]를 참고해서 사용자의 질문에 답변해주세요
-    - 결과만 출력해주세요
+def get_rag_chain():
+    llm = get_llm()
+    retriever = get_retriever()
 
-    [Context]
-    {retrieved_docs}
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is"
+    )
 
-    Question: {new_question}
-    """
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ]
+    )
 
-def  get_ai_message(user_message):
-    retrieved_docs = get_retrieved_docs(user_message) # query에 적합하게 임베딩된 결과
-    new_question = get_new_question(user_message)
-    qa_prompt = get_qa_prompt(retrieved_docs, new_question)
-    ai_message = llm.invoke(prompt)
-    return ai_message.content
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    ).pick('answer')
+
+    return conversational_rag_chain
+
+
+def get_ai_response(user_message):
+    dictionary_chain = get_dictionary_chain()
+    rag_chain = get_rag_chain()
+    tax_chain = {"input": dictionary_chain} | rag_chain
+    ai_response = tax_chain.stream(
+        {"question": user_message},
+        config={
+            "configurable": {"session_id": "abc123"}
+        }
+    )
+    return ai_response
